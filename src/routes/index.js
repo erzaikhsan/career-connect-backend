009e440900c494d46bdca7cbab6bf33501003078
erzaikhsan <erzacombas354@gmail.com>
2024-08-17 11:58:59 +0700

    config

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,7 @@ const jobsRoutes = require('./jobs.routes');
 const authRoute = require('./auth.routes');
 const companiesRoutes = require('./companies.routes');
 const ProfileRoutes = require('./user-profile.routes');
-const PhotoRoutes = require('./photo-profile.routes');
+// const PhotoRoutes = require('./photo-profile.routes');
 const applicationsRoutes = require('./applications.routes');
 const usersRoutes = require('./users.routes');
 
@@ -12,7 +12,7 @@ router.use('/jobs', jobsRoutes);
 router.use('/auth', authRoute);
 router.use('/companies', companiesRoutes);
 router.use('/jobseekers', ProfileRoutes);
-router.use('/photo', PhotoRoutes);
+// router.use('/photo', PhotoRoutes);
 router.use('/apply', applicationsRoutes);
 router.use('/users', usersRoutes);
 
--- src/routes/user-profile.routes.js
+++ src/routes/user-profile.routes.js
@@ -12,13 +12,13 @@ const {
 const { validator } = require('../middlewares');
 const { ROLES } = require('../utils/constants');
 const path = require('path');
-const logger = require('../utils/logger');
+// const logger = require('../utils/logger');
 
 const router = express.Router();
 const { validate, requirements } = validator;
 const uploadCV = StorageCV;
 const uploadCertificate = StorageCertificates;
-const uploadPath = path.join(__dirname, '../../public/uploads');
+// const uploadPath = path.join(__dirname, '../../public/uploads');
 
 router.get('/:id', UserControllers.GetUserProfile);
 router.put(
@@ -60,30 +60,30 @@ router.delete(
   CertificatesControllers.DeleteCertificates
 );
 
-router.get('/certificates/:id/:filename', (req, res) => {
-  const filename = req.params.filename;
-  const userId = req.params.id;
-  const filePath = path.join(uploadPath, `${userId}/certificates`, filename);
-  res.sendFile(filePath, (err) => {
-    if (err) {
-      logger.error(err);
-      res.status(500).end();
-    }
-  });
-});
+// router.get('/certificates/:id/:filename', (req, res) => {
+//   const filename = req.params.filename;
+//   const userId = req.params.id;
+//   const filePath = path.join(uploadPath, `${userId}/certificates`, filename);
+//   res.sendFile(filePath, (err) => {
+//     if (err) {
+//       logger.error(err);
+//       res.status(500).end();
+//     }
+//   });
+// });
 
-router.get('/cv/:id/:filename', (req, res) => {
-  const filename = req.params.filename;
-  const userId = req.params.id;
-  logger.info(filename);
-  logger.info(userId);
-  const filePath = path.join(uploadPath, `${userId}`, filename);
-  res.sendFile(filePath, (err) => {
-    if (err) {
-      logger.error(err);
-      res.status(500).end();
-    }
-  });
-});
+// router.get('/cv/:id/:filename', (req, res) => {
+//   const filename = req.params.filename;
+//   const userId = req.params.id;
+//   logger.info(filename);
+//   logger.info(userId);
+//   const filePath = path.join(uploadPath, `${userId}`, filename);
+//   res.sendFile(filePath, (err) => {
+//     if (err) {
+//       logger.error(err);
+//       res.status(500).end();
+//     }
+//   });
+// });
 
 module.exports = router;
