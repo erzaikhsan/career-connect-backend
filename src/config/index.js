e466734293fa2e93889b30186365a765ea0608af
erzaikhsan <erzacombas354@gmail.com>
2024-08-17 09:53:41 +0700

    config

--- src/config/index.js
+++ src/config/index.js
@@ -1,19 +1,22 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const db = new Sequelize({
-  dialect: 'postgres',
-  host: process.env.POSTGRES_HOST,
-  // port: process.env.DB_PORT,
-  username: process.env.POSTGRES_USER,
-  password: process.env.POSTGRES_PASSWORD,
-  database: process.env.POSTGRES_DATABASE,
-  dialectOptions: {
-    ssl: {
-      rejectUnauthorized: false
-    }
-  }
-});
+// const db = new Sequelize({
+//   dialect: 'postgres',
+//   host: process.env.DB_HOST,
+//   port: process.env.DB_PORT,
+//   username: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+//   dialectOptions: {
+//     ssl: {
+//       rejectUnauthorized: false
+//     }
+//   }
+// });
+const db = new Sequelize(
+  'postgres://default:MQKTvuNFl8z4@ep-long-mountain-a4jewc8j-pooler.us-east-1.aws.neon.tech/verceldb?sslmode=require'
+);
 
 const port = process.env.PORT_APP || 3000;
 const api = process.env.API_PATH || 'api/v1';
